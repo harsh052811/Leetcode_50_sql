36a6ed30af6c75ee46bbf1801fc9cb1b43a9d0cb
Harsh Wani <49159215+harsh052811@users.noreply.github.com>
2024-12-23 16:47:57 -0500

    Time: 910 ms (25.35%), Space: 0B (100%) - LeetHub

---
+++ 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
@@ -0,0 +1,3 @@
+select round(sum(player_login)/count(distinct player_id),2) as fraction from
+(SELECT player_id, datediff(event_date, min(event_date) over(partition by player_id)) = 1 as player_login 
+FROM activity) as new_table
